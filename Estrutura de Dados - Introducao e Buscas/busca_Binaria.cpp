6b36b3e3f2b9610149a89a60bfccf1109bb36c64
Mateus Marques <81566403+mateusmarquessz@users.noreply.github.com>
2023-05-15 11:02:42 -0300

    att

--- Estrutura de Dados - Introducao e Buscas/busca_Binaria.cpp
+++ Estrutura de Dados - Introducao e Buscas/busca_Binaria.cpp
@@ -1,13 +1,100 @@
 #include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include <fstream>
-#include <string>
 #include <iostream>
-
-using namespace std; // para utilizar cout
-
+#include <new>
+#include <string>
+#include <stdlib.h>
+#define TAM 10
+ 
+using namespace std; //para utilizar cout
+ 
+void imprime_vetor(int vetor[TAM]){
+ 
+    //Auxiliar contador
+    int cont;
+ 
+     //Imprime o vetor
+    for(cont = 0; cont < TAM; cont++){
+        cout << vetor[cont] << " - ";
+    }
+ 
+}
+ 
+int busca_simples(int vetor[TAM], int valorProcurado, int *posicaoEncontrada){
+ 
+    //Auxiliar contador
+    int cont;
+ 
+    //Verifica se o valor foi encontrado
+    bool valorFoiEncontrado;
+ 
+    //Percorre a lista em busca do valor
+    for(cont = 0; cont < TAM; cont++){
+        if(vetor[cont] == valorProcurado){
+            valorFoiEncontrado = true;
+            *posicaoEncontrada = cont;
+        }
+    }
+ 
+    if(valorFoiEncontrado){
+        return 1;
+    }else{
+        return -1;
+    }
+}
+ 
+int busca_binaria(int vetor[TAM], int valorProcurado, int *posicaoEncontrada){
+ 
+    int esquerda = 0; //Limite da esquerda 0 pq antes de chegar meio precisa saber o inicio do vetor
+    int direita = TAM - 1; //Limite da direita o ultimo elemento da lista pq vetor comeca na posicao 0
+    int meio; //O meio onde fica o cursor. ou pivo
+ 
+    while(esquerda <= direita){
+ 
+        //Encontra o meio da análise
+        meio = (esquerda + direita)/2; // escontrando o meio do vetor
+ 
+        //Quando o valor do meio é encontrado
+        if(valorProcurado == vetor[meio]){ 
+            *posicaoEncontrada = meio;
+            return 1;
+        }
+ 
+        //Ajustando os limites laterais
+        if(vetor[meio] < valorProcurado){
+            esquerda = meio + 1;
+        }else{
+            direita = meio - 1;
+        }
+ 
+    }
+ 
+    return -1;
+ 
+}
+ 
 int main(){
-
+ 
+    int vetor[TAM] = {1,23,44,56,63,72,84,90,98};
+    int valorProcurado;
+    int posicao, posicaoEncontrada;
+ 
+    //Auxiliar contador
+    int cont;
+ 
+    //Imprime o vetor na tela
+    imprime_vetor(vetor);
+ 
+    //Lendo o número buscado
+    printf("Qual numero deseja encontrar?");
+    scanf("%d", &valorProcurado);
+ 
+ 
+    if(busca_binaria(vetor, valorProcurado, &posicaoEncontrada) == 1){
+        cout << "O valor foi encontrado na posicao:" << posicaoEncontrada;
+    }else{
+        cout << "Valor nao encontrado";
+    }
+ 
     return 0;
+ 
 }
---
+++ Estrutura de Dados - Pilhas/Pilhas.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <iostream>
+#include <string>
+#include <stdlib.h>
+#define TAM 10
+ 
+using namespace std;
+
+int main(){
+    
+}
