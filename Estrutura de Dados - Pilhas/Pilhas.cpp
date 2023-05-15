801b2c9cd23f93eb6e60b55af9e3e0bd2a775029
Mateus Marques <81566403+mateusmarquessz@users.noreply.github.com>
2023-05-15 20:42:29 -0300

    att

--- Estrutura de Dados - Pilhas/Pilhas.cpp
+++ Estrutura de Dados - Pilhas/Pilhas.cpp
@@ -6,6 +6,42 @@
  
 using namespace std;
 
-int main(){
-    
+void imprimeVetor(int pilha[TAM]){
+
+    int i;
+    //imprimindo vetor
+    printf("\n");
+    for (i = 0; i < TAM; i++){
+        printf("%d-", pilha[i]);
+    }
+}
+
+int pilha_push(int pilha[TAM], int valor, int *topo){
+    //caso nao possa colocar mais valores
+    if(*topo == TAM -1){
+        cout << "Pilha cheia!";
+    } else {
+        *topo += 1;
+        pilha[*topo] = valor;
+    }
+}
+
+int main(){ 
+    int pilha[TAM] = {0,0,0,0,0,0,0,0,0,0}; //pinha de tamanho TAM
+    int topo = -1; // topo da pilha
+    imprimeVetor(pilha);
+
+    pilha_push(pilha, 5, &topo);
+    pilha_push(pilha, 7, &topo);
+    pilha_push(pilha, 5, &topo);
+    pilha_push(pilha, 7, &topo);
+    pilha_push(pilha, 5, &topo);
+    pilha_push(pilha, 7, &topo);
+    pilha_push(pilha, 5, &topo);
+    pilha_push(pilha, 7, &topo);
+    pilha_push(pilha, 5, &topo);
+    pilha_push(pilha, 7, &topo);
+
+    imprimeVetor(pilha);
+
 }
