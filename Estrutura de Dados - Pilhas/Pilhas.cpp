2e05f7f55529c6a34374e1b6ecfa27ee9235055e
Mateus Marques <81566403+mateusmarquessz@users.noreply.github.com>
2023-05-16 11:59:01 -0300

    att

--- Estrutura de Dados - Pilhas/Pilhas.cpp
+++ Estrutura de Dados - Pilhas/Pilhas.cpp
@@ -16,7 +16,7 @@ void imprimeVetor(int pilha[TAM]){
     }
 }
 
-int pilha_push(int pilha[TAM], int valor, int *topo){
+int pilha_push(int pilha[TAM], int valor, int *topo){ // coloca elementos na lista
     //caso nao possa colocar mais valores
     if(*topo == TAM -1){
         cout << "Pilha cheia!";
@@ -26,22 +26,33 @@ int pilha_push(int pilha[TAM], int valor, int *topo){
     }
 }
 
+void pilha_pop(int pilha[TAM], int *topo){ // Remove elementos da lista
+
+    //Verificar se a pilha ja esta vazia
+    if(*topo == -1){
+        cout << "A pilha ja esta vazia";
+    } else {
+        cout << "\nValor removido:" << pilha[*topo]; // mostro o que tirei
+        pilha[*topo] = 0; // serve para remover ele do vetor
+        *topo -= -1;
+    }
+
+
+}
+
+
 int main(){ 
     int pilha[TAM] = {0,0,0,0,0,0,0,0,0,0}; //pinha de tamanho TAM
-    int topo = -1; // topo da pilha
+    int topo = -1, valorRetirado; // topo da pilha -1 por que esta vazia
     imprimeVetor(pilha);
 
     pilha_push(pilha, 5, &topo);
+    imprimeVetor(pilha);
     pilha_push(pilha, 7, &topo);
+    imprimeVetor(pilha);
     pilha_push(pilha, 5, &topo);
-    pilha_push(pilha, 7, &topo);
-    pilha_push(pilha, 5, &topo);
-    pilha_push(pilha, 7, &topo);
-    pilha_push(pilha, 5, &topo);
-    pilha_push(pilha, 7, &topo);
-    pilha_push(pilha, 5, &topo);
-    pilha_push(pilha, 7, &topo);
-
+    imprimeVetor(pilha);
+    pilha_pop(pilha, &topo);
     imprimeVetor(pilha);
 
 }
