cfb1cc5d9e3b7245ce8051d773f4f3a9efec7d58
Mateus Marques <81566403+mateusmarquessz@users.noreply.github.com>
2023-05-17 14:29:01 -0300

    att

---
+++ Estrutura de Dados - Filas/filas.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <iostream>
+#include <string>
+#include <stdlib.h>
+#define TAM 10
+ 
+using namespace std;
+
+int main(){
+
+    int frente, tras;
+    int fila{TAM};
+    int valor;
+}
