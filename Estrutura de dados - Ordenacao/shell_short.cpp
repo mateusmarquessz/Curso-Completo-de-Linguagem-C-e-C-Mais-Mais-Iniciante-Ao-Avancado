efd79d9f51edf7580495bd00bea82ea09e4c6e5d
Mateus Marques <81566403+mateusmarquessz@users.noreply.github.com>
2023-05-19 19:26:00 -0300

    att

---
+++ Estrutura de dados - Ordenacao/shell_short.cpp
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <iostream>
+#include <string>
+#include <stdlib.h>
+#define TAM 10
+
+using namespace std;
+
+void imprimeVetor(int vetor[]){
+    int i;
+    cout << "\n";
+    for (i = 0; i < TAM; i++){
+        cout << " |" << vetor[i] << "| ";
+    }
+}
+
+void shell_sort(vetor[TAM]){
+
+}
+
+
+int main(){
+    int vetor[TAM] = {10,9,8,7,6,5,4,3,2,1};
+
+    shell_sort(vetor);
+
+    imprimeVetor(vetor);
+}
